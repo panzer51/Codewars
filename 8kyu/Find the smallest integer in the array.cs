2dfe43b67bb85314b773688b127dd3897532ae6c
Dominik <panzer511111@gmail.com>
2023-08-01 01:22:44 +0200

    Create Find the smallest integer in the array.cs

---
+++ 8kyu/Find the smallest integer in the array.cs
@@ -0,0 +1,28 @@
+/*
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+//Solution
+
+public class Kata 
+{
+    public static int FindSmallestInt(int[] args) 
+    {
+       int smallest = args[0];
+        for (int i = 1; i < args.Length;  i++)
+        {
+            if (args[i] < smallest)
+            {
+                smallest = args[i];
+            }
+        }
+        return smallest;
+    }
+}
+
