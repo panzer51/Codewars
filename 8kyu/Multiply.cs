9878251e7d4f1ae8b4af5cefb2a3fb2942afab3e
Dominik <panzer511111@gmail.com>
2023-07-31 19:53:46 +0200

    Create Multiply.cs

---
+++ 8kyu/Multiply.cs
@@ -0,0 +1,16 @@
+/*
+This code does not execute properly. Try to figure out why.
+
+public class CustomMath {
+    public static int multiply(int a, int b) {
+        return a b;
+    }
+}
+*/
+
+//Solution
+public class CustomMath {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
