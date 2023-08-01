8ce1e99819061d56f592f7306079eea98e87be08
Dominik <panzer511111@gmail.com>
2023-08-01 12:38:40 +0200

    Create Opposite number.cs

---
+++ 8kyu/Opposite number.cs
@@ -0,0 +1,37 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+//Solution 
+
+using System;
+
+public class Kata
+    {
+        public static int Opposite(int number)
+        {
+            // Happy Coding
+            return -number;
+        }
+    }
+
+// Another Solution 
+
+/*
+using System;
+
+public class Kata
+    {
+        public static int Opposite(int number)
+        {
+            // Happy Coding
+            return number * -1;
+        }
+    }
+*/
